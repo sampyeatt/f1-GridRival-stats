5da314b52ba9def0c0cadec05ed788a54471a99b
spyeatt <sampyeatt@gmail.com>
2025-08-07 10:17:58 -0400

    frontend doodads

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import {DashboardComponent} from './pages/dashboard/dashboard.component'
+import {DashboardComponent} from './pages/dashboard/dashboard'
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent},
