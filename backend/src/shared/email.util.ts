daf5854092f8a70b75a689ac4c1acd7cfa3e9c43
spyeatt <sampyeatt@gmail.com>
2025-08-25 13:30:56 -0400

    update button

--- backend/src/shared/email.util.ts
+++ backend/src/shared/email.util.ts
@@ -9,7 +9,7 @@ export function sendConfirmationEmail(email: string, token: string) {
     const html = `
 <h1>Confirm your email</h1>
 <p>Click the link below to confirm your email</p>
-<a href="http://localhost:3000/api/auth/confirm-email/${token}">Confirm email</a>
+<a href="http://192.168.0.151:8080/api/auth/confirm-email/${token}">Confirm email</a>
 `
 
     const resend = new Resend(process.env.RESEND_API_KEY!)
