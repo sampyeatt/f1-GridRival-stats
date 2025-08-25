61720fbd00f6110a58b1048aa4edf83602a27d89
spyeatt <sampyeatt@gmail.com>
2025-08-25 13:59:42 -0400

    update button

--- backend/src/services/team.service.ts
+++ backend/src/services/team.service.ts
@@ -7,9 +7,8 @@ export async function getAllTeams() {
 
 export async function addTeam(teamId: string, teamName: string) {
     const team = new Team()
-    team.teamId = teamId
-    team.teamName = teamName
-    return Team.build(team).save()
+    team.set({teamId, teamName})
+    return team.save()
 }
 
 export async function getTeamById(id: string) {
